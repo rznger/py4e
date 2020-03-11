726288c89c548a887a3776b37a97856ae4bfb2d2
renger jestre <rengerjestre@rengers-MacBook-Air.local>
2020-03-11 19:33:14 +0800

    final na

--- cse2.py
+++ cse2.py
@@ -2,23 +2,21 @@ hours=input("Enter Work Hours: ")
 try:
     hours=float(hours)
 except:
-    print("Please input numeric data.")
+    print("ERROR, please enter numeric input.")
     exit()
-
 rate=input("Enter Rate: ")
-try:
-    rate=float(rate)
-except:
-    print("Please input numeric data.")
-    exit()
 
-def computepay(hours, rate):
+try:
+    hours=float(hours)
+    rate=float(rate)
+
+    if hours<=40:
+        pay=hours*rate
     if hours>40:
         pay=(hours-40)*1.5*rate+40*rate
-        return pay
-    else:
-        pay=hours*rate
-        return pay
 
-x=computepay(hours, rate)
-print("Your Pay is: " + str(x))
+    print("Your Pay is: " + str(pay))
+
+except:
+    print("ERROR, please enter numeric input.")
+exit()
