066ec45e5571830782bf291e2cd056cd93f5c311
renger jestre <rengerjestre@rengers-MacBook-Air.local>
2020-03-11 19:20:43 +0800

    final na talaga

--- CSE1.py
+++ CSE1.py
@@ -1,17 +1,19 @@
-hours=input("Enter Hours: ")
+hours=input("Enter Work Hours: ")
 rate=input("Enter Rate: ")
+
 
 try:
 
     hours=float(hours)
     rate=float(rate)
 
+    if hours<=40:
+        pay=hours*rate
     if hours>40:
         pay=(hours-40)*1.5*rate+40*rate
-        return pay
-    else:
-        pay=hours*rate
-        return pay
-    float(pay)
 
-print("Pay: " + str(pay))
+    print("Your Pay is: " + str(pay))
+
+except:
+    print("ERROR, please input numeric data")
+exit()
